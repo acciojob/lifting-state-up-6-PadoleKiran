d44c83be5dd8d8b01a1de4129a620524889ac5b6
Kiran <padolekiran2001@address.com>
2025-12-06 23:28:51 +0530

    task compleated

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -20,32 +20,9 @@ export default function TodoList({ todos, handleComplete }) {
             {!todo.completed && (
               <button onClick={() => handleComplete(todo.id)}>Complete</button>
             )}
-
-            {todo.completed && <span></span>}
           </li>
         ))}
       </ul>
     </div>
   );
 }
-
-
-
-// import React from "react"
-// export default function TodoList({todos, handleComplete}){
-//     return(
-//         <div>
-//             <h2>Todo List</h2>
-
-//             {todos.map((todo) => (
-//                 <div key={todo.id}>
-//                     <span style={{textDecoration: todo.compleated ? "line-through" : "none", marginRight: "10px"}}>{todo.title}</span>
-
-//                     {!todo.completed && (<button onClick={()=>{handleComplete(todo.id)}}>Complete</button>)}
-
-//                     {todo.completed && <span></span>}
-//                 </div>
-//             ))}
-//         </div>
-//     )
-// }
