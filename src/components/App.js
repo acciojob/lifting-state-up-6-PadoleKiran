6bde9c15b9d3d9bac51772dc5eee5008856d4aab
Kiran <padolekiran2001@address.com>
2025-12-07 10:57:50 +0530

    task compleated

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,27 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
 import TodoList from "./TodoList";
 
 const App = () => {
-  let [todos,setTodos] = useState(
-    [
-      {id: 1, title: "Learn React", completed: false},
-      {id: 2, title: "Practice JavaScript", completed: false},
-      {id: 3, title: "Build a project", completed: false},
-    ]
-  );
+  const [todos, setTodos] = useState([
+    { id: 1, title: "Learn React", completed: false },
+    { id: 2, title: "Practice JavaScript", completed: false },
+    { id: 3, title: "Build a project", completed: false }
+  ]);
 
-  function handleComplete(id){
-    // const updatedTodos = todos.map((todo)=>(
-    //   todo.id === id ? {...todo, completed: true} : todo
-    // ))
-    // setTodos(updatedTodos);
-
-    setTodos((prevTodo) => 
-      prevTodo.map((todo)=>
-        todo.id === id ? {...todo, completed: true} : todo
+  function handleComplete(id) {
+    setTodos(prev =>
+      prev.map(todo =>
+        todo.id === id ? { ...todo, completed: true } : todo
       )
-    )
+    );
   }
-  
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        {/* <h1>Test</h1> */}
-        <h1>Todo App</h1>
-        <TodoList todos={todos} handleComplete={handleComplete} />
-
+      <h1>Todo App</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,27 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function TodoList({ todos, handleComplete }) {
-  const [clicked, setClicked] = useState(false);
-
-  const onClick = (id) => {
-    handleComplete(id);
-    setClicked(true); // hide all buttons after first click (what Cypress expects)
-  };
-
   return (
     <div>
       <h2>Todo List</h2>
 
       <ul>
-        {todos.map((todo) => (
+        {todos.map(todo => (
           <li key={todo.id}>
-            {/* ✅ BUTTON IS FIRST DIRECT CHILD */}
-            {!clicked && (
-              <button onClick={() => onClick(todo.id)}>Complete</button>
-            )}
+            <button
+              onClick={() => handleComplete(todo.id)}
+              style={{ display: todo.completed ? "none" : "inline" }}
+            >
+              Complete
+            </button>
 
-            {/* Title AFTER the button */}
-            <span style={{ marginLeft: "8px" }}>{todo.title}</span>
+            {todo.title}
           </li>
         ))}
       </ul>
